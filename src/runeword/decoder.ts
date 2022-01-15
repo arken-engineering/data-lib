54d40d9572ea3b98d04765a6be1feb66e3fdfc61
grinnfold <grinnfold@protonmail.com>
2022-01-15 21:48:46 +0100

    Fix compiler

--- src/runeword/decoder.ts
+++ src/runeword/decoder.ts
@@ -1,32 +1,13 @@
 import { Runeword } from ".";
-import { ItemsMainCategoriesType } from "../categories";
 import { Branches } from "../data";
 import { ItemTypeID } from "../type";
 import { buildRarity, createRunewordModsMerger, getAttributes, getModsFromToken, getPerfectionAndShorthand, includeCommunityRequests } from "./decoder-parts";
 import { createEmptyItem } from "./runeword";
 
 
-export function decodeItem(tokenId: string) {
-    if (!tokenId || parseInt(tokenId) === 0 || Number.isNaN(parseInt(tokenId))) return {
-        tokenId,
-        details: {},
-        branches: {},
-        shorthand: '',
-        mods: [],
-        attributes: [],
-        perfection: null,
-        category: ItemsMainCategoriesType.WEAPONS,
-        slots: [],
-        meta: {
-          harvestYield: 0,
-          harvestFeeToken: '',
-          harvestFeePercent: 0,
-          harvestFees: {},
-          chanceToSendHarvestToHiddenPool: 0,
-          chanceToLoseHarvest: 0,
-          harvestBurn: 0,
-        },
-      }
+export function decodeItem(tokenId: string): Runeword {
+    if (!tokenId || parseInt(tokenId) === 0 || Number.isNaN(parseInt(tokenId))) 
+      return createEmptyItem(tokenId, 0, 0) as Runeword;
     
     const version = parseInt(tokenId.slice(1, 4))
     const id = parseInt(tokenId.slice(4, 9));
